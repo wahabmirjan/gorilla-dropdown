f37184759d8a8ef6adb5cf38c7c0620852a3f9b2
Wahab Mirjan <wahab.mirjan@gorilla.solutions>
2017-10-23 18:35:56 -0400

    Issue #9 - Add plugin header and license information

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var babel = require('gulp-babel');
 var cleanCSS = require('gulp-clean-css');
 var cssbeautify = require('gulp-cssbeautify');
 var del = require('del');
+var header = require('gulp-header');
 var jshint = require('gulp-jshint');
 var postcss = require('gulp-postcss');
 var rename = require('gulp-rename');
@@ -15,6 +16,9 @@ var uglify = require('gulp-uglify');
 
 
 
+
+//Package file
+var PKG = require('./package.json');
 
 // Source files
 var SRC_CSS = 'src/css/*.scss';
@@ -28,32 +32,15 @@ var DEST_JS = 'dist/js';
 
 
 
-// CLEAN files
+// ****************************** CSS ******************************
+
+// CLEAN CSS
 gulp.task('clean:css', function () {
 	return del([DEST_CSS]);
 });
 
-gulp.task('clean:js', function () {
-	return del([DEST_JS]);
-});
 
-
-
-
-
-// Lint JS
-gulp.task('lint:js', function() {
-	return gulp.src(SRC_JS)
-			.pipe(jshint())
-			.pipe(jshint.reporter('default'))
-			.pipe(jshint.reporter('fail'));
-});
-
-
-
-
-
-// Build CSS
+//Build CSS
 gulp.task('build:css', ['clean:css'], function () {
 	return gulp.src(SRC_CSS)
 			.pipe(sass().on('error', sass.logError))
@@ -65,10 +52,43 @@ gulp.task('build:css', ['clean:css'], function () {
 			.pipe(gulp.dest(DEST_CSS));
 });
 
+
+
+
+
+// ****************************** JS ******************************
+
+// CLEAN JS
+gulp.task('clean:js', function () {
+	return del([DEST_JS]);
+});
+
+
+// Lint JS
+gulp.task('lint:js', function() {
+	return gulp.src(SRC_JS)
+			.pipe(jshint())
+			.pipe(jshint.reporter('default'))
+			.pipe(jshint.reporter('fail'));
+});
+
+
+// Banner for JS file
+var banner = ['/**',
+		' * <%= pkg.name %>',
+		' * Description - <%= pkg.description %>',
+		' * @version v<%= pkg.version %>',
+		' * @link <%= pkg.homepage %>',
+		' * @license <%= pkg.license %>',
+		' */',
+		'',
+		''].join('\n');
+
 // Build JS
 gulp.task('build:js', ['clean:js', 'lint:js'], function() {
 	return gulp.src(SRC_JS)
 			.pipe(babel({presets: ['env']}))
+			.pipe(header(banner, { pkg : PKG } ))
 			.pipe(gulp.dest(DEST_JS))
 			.pipe(uglify({output: {comments:'some'}}))
 			.pipe(rename({suffix: '.min'}))
@@ -79,8 +99,12 @@ gulp.task('build:js', ['clean:js', 'lint:js'], function() {
 
 
 
+// ****************************** WATCH ******************************
+
 // WATCH for file changes and run the tasks
 gulp.task('watch', function() {
     gulp.watch(SRC_JS, ['build:js']);
     gulp.watch(SRC_CSS, ['build:css']);
 });
+
+
